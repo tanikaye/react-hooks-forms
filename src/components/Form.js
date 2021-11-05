15ff8242b61ebf4e860fc7b5f1bddf481fa50e51
Tani Kaye <1tanikaye@gmail.com>
2021-11-05 11:30:41 -0400

    commit tree a977019e9898f978099d7e379c43368469d19591

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,13 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
+      <input type="text" value={firstName} onChange={handleFirstNameChange}/>
       <input type="text" value={lastName} />
       <button type="submit">Submit</button>
     </form>
